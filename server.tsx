de6160d0c6fce4552987a72e36b8139be9a0d8a4
Lucks <lucks17@gmail.com>
2026-04-12 11:47:20 -0600

    add public directory

--- server.tsx
+++ server.tsx
@@ -2,8 +2,11 @@ import React from "react";
 import * as ReactDOMServer from "react-dom/server";
 import { Resume } from "./Resume";
 import { writeFileSync } from "fs";
+import { mkdirSync } from "fs";
+
+mkdirSync("public", { recursive: true });
 
 writeFileSync(
-  "index.html",
+  "public/index.html",
   "<!DOCTYPE html>" + ReactDOMServer.renderToStaticMarkup(<Resume />)
 );
